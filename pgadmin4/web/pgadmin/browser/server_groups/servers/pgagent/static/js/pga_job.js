c20a3a12ee04df16ca9d6de59d610d59467eed99
brianchristy <brianchristopher170804@gmail.com>
2025-04-22 18:15:12 +0530

    first commit

---
+++ pgadmin4/web/pgadmin/browser/server_groups/servers/pgagent/static/js/pga_job.js
@@ -0,0 +1,242 @@
+/////////////////////////////////////////////////////////////
+//
+// pgAdmin 4 - PostgreSQL Tools
+//
+// Copyright (C) 2013 - 2025, The pgAdmin Development Team
+// This software is released under the PostgreSQL Licence
+//
+//////////////////////////////////////////////////////////////
+
+import { getNodeAjaxOptions } from '../../../../../static/js/node_ajax';
+import PgaJobSchema from './pga_job.ui';
+import { getNodePgaJobStepSchema } from '../../steps/static/js/pga_jobstep.ui';
+import getApiInstance from '../../../../../../static/js/api_instance';
+import pgAdmin from 'sources/pgadmin';
+
+define('pgadmin.node.pga_job', [
+  'sources/gettext', 'sources/url_for', 'pgadmin.browser',
+  'pgadmin.node.pga_jobstep', 'pgadmin.node.pga_schedule',
+], function(gettext, url_for, pgBrowser) {
+
+  if (!pgBrowser.Nodes['coll-pga_job']) {
+    pgBrowser.Nodes['coll-pga_job'] =
+      pgBrowser.Collection.extend({
+        node: 'pga_job',
+        label: gettext('pga_jobs'),
+        type: 'coll-pga_job',
+        columns: ['jobid', 'jobname', 'jobenabled', 'jlgstatus', 'jobnextrun', 'joblastrun', 'jobdesc'],
+        hasStatistics: false,
+        canDrop: true,
+        canDropCascade: false,
+      });
+  }
+
+  if (!pgBrowser.Nodes['pga_job']) {
+    pgBrowser.Nodes['pga_job'] = pgBrowser.Node.extend({
+      parent_type: 'server',
+      type: 'pga_job',
+      dialogHelp: url_for('help.static', {'filename': 'pgagent_jobs.html'}),
+      hasSQL: true,
+      hasDepends: false,
+      hasStatistics: true,
+      hasCollectiveStatistics: true,
+      width: '80%',
+      height: '80%',
+      canDrop: true,
+      label: gettext('pgAgent Job'),
+      node_image: function() {
+        return 'icon-pga_job';
+      },
+      Init: function() {
+        /* Avoid mulitple registration of menus */
+        if (this.initialized)
+          return;
+
+        this.initialized = true;
+
+        pgBrowser.add_menus([{
+          name: 'create_pga_job_on_coll', node: 'coll-pga_job', module: this,
+          applies: ['object', 'context'], callback: 'show_obj_properties',
+          category: 'create', priority: 4, label: gettext('pgAgent Job...'),
+          data: {action: 'create'},
+        },{
+          name: 'create_pga_job', node: 'pga_job', module: this,
+          applies: ['object', 'context'], callback: 'show_obj_properties',
+          category: 'create', priority: 4, label: gettext('pgAgent Job...'),
+          data: {action: 'create'},
+        }, {
+          name: 'run_now_pga_job', node: 'pga_job', module: this,
+          applies: ['object', 'context'], callback: 'run_pga_job_now',
+          priority: 4, label: gettext('Run now'), data: {action: 'create'},
+        }, {
+          name: 'job_audit_log', node: 'coll-pga_job', module: this,
+          applies: ['object', 'context'], callback: 'show_job_audit_log',
+          priority: 5, label: gettext('Job Audit Log (All)'), icon: 'fa fa-history',
+        }, {
+          name: 'job_audit_log_create', node: 'coll-pga_job', module: this,
+          applies: ['object', 'context'], callback: 'show_job_audit_log_create',
+          priority: 5, label: gettext('Job Audit Log (Create)'), icon: 'fa fa-plus',
+        }, {
+          name: 'job_audit_log_modify', node: 'coll-pga_job', module: this,
+          applies: ['object', 'context'], callback: 'show_job_audit_log_modify',
+          priority: 5, label: gettext('Job Audit Log (Modify)'), icon: 'fa fa-edit',
+        }, {
+          name: 'job_audit_log_delete', node: 'coll-pga_job', module: this,
+          applies: ['object', 'context'], callback: 'show_job_audit_log_delete',
+          priority: 5, label: gettext('Job Audit Log (Delete)'), icon: 'fa fa-trash',
+        }, {
+          name: 'job_audit_log_execute', node: 'coll-pga_job', module: this,
+          applies: ['object', 'context'], callback: 'show_job_audit_log_execute',
+          priority: 5, label: gettext('Job Audit Log (Execute)'), icon: 'fa fa-play',
+        }]);
+      },
+
+      getSchema: function(treeNodeInfo, itemNodeData) {
+        return new PgaJobSchema(
+          {
+            jobjclid: ()=>getNodeAjaxOptions('classes', this, treeNodeInfo, itemNodeData, {
+              cacheLevel: 'server',
+              cacheNode: 'server'
+            }),
+            jobs: ()=>getNodeAjaxOptions('jobs', this, treeNodeInfo, itemNodeData, {
+              cacheLevel: 'server',
+              cacheNode: 'server',
+              urlWithId: true,
+              useCache: false
+            })
+          },
+          () => getNodePgaJobStepSchema(treeNodeInfo, itemNodeData),
+        );
+      },
+
+      /* Run pgagent job now */
+      run_pga_job_now: function(args) {
+        let input = args || {},
+          obj = this,
+          t = pgBrowser.tree,
+          i = input.item || t.selected(),
+          d = i  ? t.itemData(i) : undefined;
+
+        if (d) {
+          getApiInstance().put(
+            obj.generate_url(i, 'run_now', d, true),
+          ).then(({data: res})=> {
+            pgAdmin.Browser.notifier.success(res.info);
+            t.unload(i);
+          }).catch(function(error) {
+            pgAdmin.Browser.notifier.pgRespErrorNotify(error);
+            t.unload(i);
+          });
+        }
+
+        return false;
+      },
+      /* Common function for all Job Audit Log operations */
+      _copy_job_audit_log_query: function(args, operation) {
+        let input = args || {},
+          t = pgBrowser.tree,
+          i = input.item || t.selected(),
+          d = i ? t.itemData(i) : undefined;
+
+        if (!d) {
+          pgAdmin.Browser.notifier.error(gettext('No item selected.'));
+          return false;
+        }
+
+        // Find the server node so we know what database to connect to
+        let serverNode = i;
+        let serverData = d;
+        
+        if (d._type !== 'server') {
+          // Navigate up to find the server
+          while (serverData && serverData._type !== 'server' && serverNode) {
+            serverNode = t.parent(serverNode);
+            if (serverNode) {
+              serverData = t.itemData(serverNode);
+            }
+          }
+        }
+
+        if (!serverNode || !serverData || serverData._type !== 'server') {
+          pgAdmin.Browser.notifier.error(gettext('Could not find server node.'));
+          return false;
+        }
+
+        try {
+          // Build the query based on operation type
+          let query = 'SELECT * FROM pgagent.pga_job_audit_log';
+          
+          // Add WHERE clause if operation is specified
+          if (operation) {
+            query += ' WHERE operation_type = ' + '\''+operation + '\'';
+          }
+          
+          // Add ORDER BY clause
+          query += ' ORDER BY operation_time DESC;';
+          
+          // Create a temporary textarea to copy the query
+          const tempTextArea = document.createElement('textarea');
+          tempTextArea.value = query;
+          document.body.appendChild(tempTextArea);
+          tempTextArea.select();
+          document.execCommand('copy');
+          document.body.removeChild(tempTextArea);
+          
+          // Show success message with operation type
+          let operationLabel = operation ? operation : 'All';
+          pgAdmin.Browser.notifier.success(
+            gettext('SQL query for ' + operationLabel + ' operations copied to clipboard. Paste into the query tool and execute.')
+          );
+          
+          // Now open the query tool 
+          pgAdmin.Browser.Node.callbacks.show_query_tool({}, serverNode);
+          
+        } catch (error) {
+          pgAdmin.Browser.notifier.error(gettext('Error: ') + error.message);
+        }
+        
+        return false;
+      },
+
+      /* Show Job Audit Log - All operations */
+      show_job_audit_log: function(args) {
+        return this._copy_job_audit_log_query(args, null);
+      },
+      
+      /* Show Job Audit Log - Create operations */
+      show_job_audit_log_create: function(args) {
+        return this._copy_job_audit_log_query(args, 'CREATE');
+      },
+      
+      /* Show Job Audit Log - Modify operations */
+      show_job_audit_log_modify: function(args) {
+        return this._copy_job_audit_log_query(args, 'MODIFY');
+      },
+      
+      /* Show Job Audit Log - Delete operations */
+      show_job_audit_log_delete: function(args) {
+        return this._copy_job_audit_log_query(args, 'DELETE');
+      },
+      
+      /* Show Job Audit Log - Execute operations */
+      show_job_audit_log_execute: function(args) {
+        return this._copy_job_audit_log_query(args, 'EXECUTE');
+      },
+      /* Refresh the jobs list after creating a new job */
+      onSave: function(isNew, data) {
+        let obj = this;
+        return obj.nodeAjax('create', data, {}, function(res) {
+          if (res.success) {
+            // Refresh the jobs list by invalidating the cache
+            let cacheNode = pgAdmin.Browser.Nodes['server'];
+            if (cacheNode) {
+              cacheNode.cache(obj.type + '#jobs', {}, 'server', null);
+            }
+          }
+        });
+      }
+    });
+  }
+
+  return pgBrowser.Nodes['pga_job'];
+});
---
+++ pgadmin4/web/pgadmin/browser/server_groups/servers/pgagent/static/js/pga_job.ui.js
@@ -0,0 +1,313 @@
+/////////////////////////////////////////////////////////////
+//
+// pgAdmin 4 - PostgreSQL Tools
+//
+// Copyright (C) 2013 - 2025, The pgAdmin Development Team
+// This software is released under the PostgreSQL Licence
+//
+//////////////////////////////////////////////////////////////
+
+import gettext from 'sources/gettext';
+import BaseUISchema from 'sources/SchemaView/base_schema.ui';
+import PgaJobScheduleSchema from '../../schedules/static/js/pga_schedule.ui';
+import PgaJobDependencySchema from './pga_job_dependency.ui';
+
+class AuditLogSchema extends BaseUISchema {
+  get baseFields() {
+    return [
+      {
+        id: 'audit_id', label: gettext('ID'), cell: 'string', type: 'text',
+        readonly: true, mode: ['properties'], sortable: true,
+      }, {
+        id: 'operation_type', label: gettext('Operation'), cell: 'string', type: 'text',
+        readonly: true, mode: ['properties'], sortable: true,
+      }, {
+        id: 'operation_time', label: gettext('Time'), cell: 'string', type: 'text',
+        readonly: true, mode: ['properties'], sortable: true,
+      }, {
+        id: 'operation_user', label: gettext('User'), cell: 'string', type: 'text',
+        readonly: true, mode: ['properties'], sortable: true,
+      }, {
+        id: 'old_values', label: gettext('Old Values'), cell: 'string', type: 'jsonb',
+        readonly: true, mode: ['properties'],
+        controlProps: {
+          formatter: {
+            fromRaw: (value) => {
+              return value ? JSON.stringify(value, null, 4) : '';
+            }
+          }
+        }
+      }, {
+        id: 'new_values', label: gettext('New Values'), cell: 'string', type: 'jsonb',
+        readonly: true, mode: ['properties'],
+        controlProps: {
+          formatter: {
+            fromRaw: (value) => {
+              return value ? JSON.stringify(value, null, 4) : '';
+            }
+          }
+        }
+      }, {
+        id: 'additional_info', label: gettext('Additional Info'), cell: 'string', type: 'text',
+        readonly: true, mode: ['properties'],
+      }
+    ];
+  }
+}
+
+export default class PgaJobSchema extends BaseUISchema {
+  constructor(fieldOptions={}, getPgaJobStepSchema=()=>[], initValues={}) {
+    super({
+      jobname: '',
+      jobid: undefined,
+      jobenabled: true,
+      jobhostagent: '',
+      jobjclid: 1,
+      jobcreated: undefined,
+      jobchanged: undefined,
+      jobnextrun: undefined,
+      joblastrun: undefined,
+      jlgstatus: undefined,
+      jobrunningat: undefined,
+      jobdesc: '',
+      jsteps: [],
+      jschedules: [],
+      jdependencies: [],
+      ...initValues,
+    });
+
+    this.fieldOptions = {
+      jobjclid: [],
+      jobs: [],
+      ...fieldOptions,
+    };
+    this.getPgaJobStepSchema = getPgaJobStepSchema;
+  }
+
+  get idAttribute() {
+    return 'jobid';
+  }
+
+  get baseFields() {
+    return [
+      {
+        id: 'jobname', label: gettext('Name'), type: 'text', noEmpty: true,
+      },{
+        id: 'jobid', label: gettext('ID'), mode: ['properties'],
+        type: 'int',
+      },{
+        id: 'jobenabled', label: gettext('Enabled?'), type: 'switch',
+      },{
+        id: 'jobjclid', label: gettext('Job class'), type: 'select',
+        options: this.fieldOptions.jobjclid,
+        controlProps: {allowClear: false},
+        mode: ['properties'],
+      },{
+        id: 'jobjclid', label: gettext('Job class'), type: 'select',
+        options: this.fieldOptions.jobjclid,
+        mode: ['create', 'edit'],
+        controlProps: {allowClear: false},
+        helpMessage: gettext('Please select a class to categorize the job. This option will not affect the way the job runs.'),
+        helpMessageMode: ['edit', 'create'],
+      },{
+        id: 'jobhostagent', label: gettext('Host agent'), type: 'text',
+        mode: ['properties'],
+      },{
+        id: 'jobhostagent', label: gettext('Host agent'), type: 'text',
+        mode: ['edit', 'create'],
+        helpMessage: gettext('Enter the hostname of a machine running pgAgent if you wish to ensure only that machine will run this job. Leave blank if any host may run the job.'),
+        helpMessageMode: ['edit', 'create'],
+      },{
+        id: 'jobcreated', type: 'text', mode: ['properties'],
+        label: gettext('Created'),
+      },{
+        id: 'jobchanged', type: 'text', mode: ['properties'],
+        label: gettext('Changed'),
+      },{
+        id: 'jobnextrun', type: 'text', mode: ['properties'],
+        label: gettext('Next run'),
+      },{
+        id: 'joblastrun', type: 'text', mode: ['properties'],
+        label: gettext('Last run'),
+      },{
+        id: 'jlgstatus', type: 'text', label: gettext('Last result'), mode: ['properties'],
+        controlProps: {
+          formatter: {
+            fromRaw: (originalValue)=>{
+              return originalValue || gettext('Unknown');
+            },
+          }
+        }
+      },{
+        id: 'jobrunningat', type: 'text', mode: ['properties'], label: gettext('Running at'),
+        controlProps: {
+          formatter: {
+            fromRaw: (originalValue)=>{
+              return originalValue || gettext('Not running currently.');
+            },
+          }
+        }
+      },{
+        id: 'jobdesc', label: gettext('Comment'), type: 'multiline',
+      },{
+        id: 'jsteps', label: '', group: gettext('Steps'),
+        type: 'collection', mode: ['edit', 'create'],
+        schema: this.getPgaJobStepSchema(),
+        canEdit: true, canAdd: true, canDelete: true,
+        columns: [
+          'jstname', 'jstenabled', 'jstkind', 'jstconntype', 'jstonerror',
+        ],
+      },{
+        id: 'jschedules', label: '', group: gettext('Schedules'),
+        type: 'collection', mode: ['edit', 'create'],
+        schema: new PgaJobScheduleSchema(),
+        canAdd: true, canDelete: true, canEdit: true,
+        columns: ['jscname', 'jscenabled', 'jscstart', 'jscend'],
+      },{
+        id: 'jdependencies', label: '', group: gettext('Dependencies'),
+        type: 'collection', mode: ['edit', 'create', 'properties'],
+        schema: new PgaJobDependencySchema({
+          jobs: this.fieldOptions.jobs,
+        }),
+        canAdd: true, canDelete: true, canEdit: true,
+        columns: ['dependent_jobname'],
+        uniqueCol: ['dependent_jobid'],
+      },{
+        id: 'audit_logs', label: '', group: gettext('Audit Logs'),
+        mode: ['properties'],
+        type: 'collection',
+        schema: new AuditLogSchema(),
+        canAdd: false, canDelete: false, canEdit: false,
+        columns: ['audit_id', 'operation_type', 'operation_time', 'operation_user'],
+        url: 'audit_log',
+        // Enable sorting
+        isSortable: true,
+        sortColumns: {
+          'audit_id': 'numeric',
+          'operation_type': 'string',
+          'operation_time': 'string',
+          'operation_user': 'string'
+        },
+        // Add filter toolbar with simple filter buttons
+        customButtomPanel: true,
+        customButtomPanelRender: (panel) => {
+          const container = document.createElement('div');
+          container.className = 'pg-audit-filter-container';
+          container.style.padding = '8px';
+          container.style.display = 'flex';
+          container.style.flexWrap = 'wrap';
+          container.style.gap = '8px';
+          container.style.alignItems = 'center';
+          
+          // Create operation type filter buttons
+          const opTypeLabel = document.createElement('span');
+          opTypeLabel.textContent = 'Filter by: ';
+          opTypeLabel.style.marginRight = '8px';
+          container.appendChild(opTypeLabel);
+          
+          // Create filter buttons for operation types
+          const operations = [
+            { label: 'CREATE', value: 'CREATE' },
+            { label: 'MODIFY', value: 'MODIFY' },
+            { label: 'EXECUTE', value: 'EXECUTE' },
+          ];
+          
+          operations.forEach(op => {
+            const btn = document.createElement('button');
+            btn.className = 'btn btn-secondary btn-sm';
+            btn.textContent = op.label;
+            btn.style.marginRight = '5px';
+            btn.onclick = function() {
+              // Apply filter when button is clicked
+              panel.onCustomFilter({
+                operation_types: op.value
+              });
+            };
+            container.appendChild(btn);
+          });
+          
+          // Add a reset button
+          const resetBtn = document.createElement('button');
+          resetBtn.className = 'btn btn-secondary btn-sm';
+          resetBtn.textContent = 'Show All';
+          resetBtn.style.marginLeft = '5px';
+          resetBtn.onclick = function() {
+            // Reset filters
+            panel.onCustomFilter({});
+          };
+          container.appendChild(resetBtn);
+          
+          // Add date range filters
+          const dateContainer = document.createElement('div');
+          dateContainer.style.display = 'flex';
+          dateContainer.style.alignItems = 'center';
+          dateContainer.style.marginTop = '8px';
+          dateContainer.style.width = '100%';
+          
+          const dateLabel = document.createElement('span');
+          dateLabel.textContent = 'Date range: ';
+          dateLabel.style.marginRight = '8px';
+          dateContainer.appendChild(dateLabel);
+          
+          // Add date inputs
+          const dateFrom = document.createElement('input');
+          dateFrom.type = 'date';
+          dateFrom.className = 'form-control';
+          dateFrom.style.marginRight = '8px';
+          dateFrom.style.width = '150px';
+          dateContainer.appendChild(dateFrom);
+          
+          const dateTo = document.createElement('input');
+          dateTo.type = 'date';
+          dateTo.className = 'form-control';
+          dateTo.style.marginRight = '8px';
+          dateTo.style.width = '150px';
+          dateContainer.appendChild(dateTo);
+          
+          // Add apply button for date range
+          const applyDateBtn = document.createElement('button');
+          applyDateBtn.className = 'btn btn-primary btn-sm';
+          applyDateBtn.textContent = 'Apply Date Filter';
+          applyDateBtn.onclick = function() {
+            const params = {};
+            if (dateFrom.value) params.date_from = dateFrom.value;
+            if (dateTo.value) params.date_to = dateTo.value;
+            panel.onCustomFilter(params);
+          };
+          dateContainer.appendChild(applyDateBtn);
+          
+          container.appendChild(dateContainer);
+          
+          // Add some styling
+          const style = document.createElement('style');
+          style.textContent = `
+            .pg-audit-filter-container button:hover {
+              background-color: #4a6785;
+              color: white;
+            }
+          `;
+          container.appendChild(style);
+          
+          return container;
+        },
+        onCustomFilter: (filterValues) => {
+          const params = {};
+          
+          if (filterValues.operation_types) {
+            params.operation_types = filterValues.operation_types;
+          }
+          
+          if (filterValues.date_from) {
+            params.date_from = filterValues.date_from;
+          }
+          
+          if (filterValues.date_to) {
+            params.date_to = filterValues.date_to;
+          }
+          
+          return params;
+        },
+      }
+    ];
+  }
+}
